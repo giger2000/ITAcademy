995623a8c129e25e94df01b800d3fbc343fd91ca
giger2000 <alberto.anuncio@gmail.com>
2019-01-18 10:03:02 +0100

    Exercicis 1 i 2 de lletres

---
+++ ITAcademy Exercicis/lletres/lletres1.java
@@ -0,0 +1,17 @@
+
+public class lletres1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Inicialitzem matriu
+		String[]miArray= {"A","L","B","E","R","T"};
+		
+		
+		for(int i=0;i<miArray.length;i++) {
+			System.out.println("Lletra "+miArray[i]);
+
+		}
+
+	}
+}
